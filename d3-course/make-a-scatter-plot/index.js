e74cb22564e411f8ca62474b35e50bf07f15d6f1
lars <60745347+lars-ruijs@users.noreply.github.com>
2020-11-09 11:04:58 +0100

    Setup for scatter plot assignment

---
+++ d3-course/make-a-scatter-plot/index.js
@@ -0,0 +1,3 @@
+// Code adapted from D3 course by Curran Kelleher.
+// VIDEO: 'Making a Scatter Plot with D3.js'
+// View live on Vizhub: https://vizhub.com/lars-ruijs/846c33f62efb4ffa80e6cf98b1eab27b
