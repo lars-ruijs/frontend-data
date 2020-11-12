60dde0bc56b2ec8f57f4728d434a19b85b01a77f
lars <60745347+lars-ruijs@users.noreply.github.com>
2020-11-12 16:46:08 +0100

    Fix the update pattern. Next: combining with script.js

--- rdw-data/scripts/datavis.js
+++ rdw-data/scripts/datavis.js
@@ -1,20 +1,16 @@
-//////////////////
-/// BAR CHART ///
-/////////////////
-
-// Dummy data (based on my cleaned data from RDW), this is a smaller sample
 const dataStad = [{city: "'s-Hertogenbosch", prLocations: 5}, {city: "Purmerend", prLocations: 3}, {city: "Amsterdam", prLocations: 10}];
 
+
 // set the dimensions and margins of the graph
-const margin = {top: 20, right: 30, bottom: 80, left: 80};
+const margin = {top: 30, right: 30, bottom: 70, left: 60};
 const width = 460;
 const height = 400;
 const innerWidth = width - margin.left - margin.right;
 const innerHeight = height - margin.top - margin.bottom;
 
-dataStad.sort((b, a) => a.prLocations - b.prLocations);
+ dataStad.sort((b, a) => a.prLocations - b.prLocations);
 
-// Append the svg object to the body of the page
+// append the svg object to the body of the page
 const svg = d3.select(".charts")
   .append("svg")
     .attr("width", width)
@@ -28,32 +24,19 @@ const x = d3.scaleBand()
 	.range([0, innerWidth])
   .padding(0.2);
 
-// Add text to X axis
 svg.append("g")
   .call(d3.axisBottom(x))
-  .attr("transform", `translate(0, ${innerHeight})`)
-	.append('text')
-  .attr('class', 'axis-label')
-  .attr('y', 50)
-  .attr('x', innerWidth/2)
-  .text('Steden met P+R');
+  .attr("transform", `translate(0, ${innerHeight})`);
 
 // Add Y axis
 const y = d3.scaleLinear()
   .domain([0, d3.max(dataStad, d => d.prLocations)])
   .range([innerHeight, 0]);
 
-// Add text to Y axis
 svg.append("g")
-  .call(d3.axisLeft(y))
-	.append('text')
-  .attr('class', 'axis-label')
-  .attr('y', -40)
-  .attr('x', -innerHeight/2+5)
-  .attr("transform", `rotate(-90)`)
-  .text('Aantal P+R locaties >');
+  .call(d3.axisLeft(y));
 
-// Create the bars
+// Bars
 svg.selectAll("rect")
   .data(dataStad)
   .enter()
@@ -66,7 +49,7 @@ svg.selectAll("rect")
 		.attr("class", d => d.city)
 		.on("click", updatePie);
 
-
+//////////////////
 //////////////////
 /// PIE CHART ///
 /////////////////
@@ -92,6 +75,10 @@ const pieChart = d3.select(".charts")
 
 // Function for making the Pie chart
 function drawPie(data) {
+  
+  const arc = d3.arc()
+    .innerRadius(0)
+    .outerRadius(100);
 
   // Data inside pie chart must be generated based on the capacity
   const pie = d3.pie()
@@ -99,36 +86,177 @@ function drawPie(data) {
     .value(d => d.capacity);
 
   // Select all group elements inside the main pieChart svg element
-  const allGroups = pieChart.selectAll("g")
+  const pieGroups = pieChart.selectAll("path")
     .data(pie(data));
-
-  // Group for every single 'slice'
-  // Merge() with allGroups to draw a complete circle
-  const sliceGroup = allGroups
-    .enter()
-    .append('g')
-    .merge(allGroups);
-
-  const arc = d3.arc()
-    .innerRadius(0)
-    .outerRadius(100);
-
-  // Add path to a 'slice' group element
-  sliceGroup
-    .append('path')
+  
+  pieGroups
+  .attr('d', arc)
+    .attr("fill", (data, i)=>{ 
+            return d3.schemeTableau10[i]; 
+          });
+  
+  pieGroups
+  .enter()
+  .append('path')
     .attr('d', arc)
     .attr("fill", (data, i)=>{ 
             return d3.schemeTableau10[i]; 
           });
+  pieGroups.exit().remove();
 
-  // Add text to a 'slice' group element
-  sliceGroup
-    .append('text')
-    .attr("transform", d => `translate(${arc.centroid(d)})`)
-    .text(d => d.value)
-    .style("text-anchor", "middle");
+  const textGroups = pieChart.selectAll("text")
+      .data(pie(data));
+
+  textGroups
+  .attr('class', 'pieText')
+  .attr("transform", d => `translate(${arc.centroid(d)})`)
+  .text(d => d.value)
+  .style("text-anchor", "middle");
+  
+  textGroups
+  .enter()
+  .append('text')
+  .attr('class', 'pieText')
+  .attr("transform", d => `translate(${arc.centroid(d)})`)
+  .text(d => d.value)
+  .style("text-anchor", "middle");
+  
+ textGroups.exit().remove();
+
+
+  // Group for every single 'slice'
+  // Merge() with allGroups to draw a complete circle
+  // const sliceGroup = pieGroups
+  //   .enter()
+  //   .append('g')
+  // 	.attr('class', 'pieGroup')
+  //   .merge(pieGroups);
+  
+  // const currentPieElements = pieChart.selectAll("path")
+  // 	.attr('d', arc)
+  //   .attr("fill", (data, i)=>{ 
+  //           return d3.schemeTableau10[i]; 
+  //         });
+  
+  //d3.selectAll('.pieSlice').remove();
+  //d3.selectAll('.pieText').remove();
+
+  // Add path to a 'slice' group element
+//   sliceGroup
+//     .append('path')
+//     .attr('class', 'pieSlice')
+//     .attr('d', arc)
+//     .attr("fill", (data, i)=>{ 
+//             return d3.schemeTableau10[i]; 
+//           });
+
+//   // Add text to a 'slice' group element
+//   sliceGroup
+//     .append('text')
+//   	.attr('class', 'pieText')
+//     .attr("transform", d => `translate(${arc.centroid(d)})`)
+//     .text(d => d.value)
+//     .style("text-anchor", "middle");
 
   // Remove all unnecessary group elements
-  allGroups.exit().remove();
+ // pieGroups.exit().remove();
   
+
+							
+					
+  
+// // Select all group elements inside the main pieChart svg element
+// const allGroups = pieChart.selectAll("g")
+// 	.data(pie(data));
+
+// // Group for every single 'slice'
+// const sliceGroup = allGroups
+// 	.enter()
+//   .append('g')
+// 	.merge(allGroups)
+
+// const arc = d3.arc()
+// 	.innerRadius(0)
+// 	.outerRadius(100)
+
+// // Add path to a 'slice' group element
+// sliceGroup
+//   .append('path')
+//   .attr('d', arc)
+//   .attr("fill", (data, i)=>{ 
+//           return d3.schemeTableau10[i]; 
+//         })
+  
+// // Add text to a 'slice' group element
+// sliceGroup
+// 	.append('text')
+// 	.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")";  })
+// 	.text(function(d){ 
+// 		return d.value;  
+// 	})
+// 	.style("text-anchor", "middle")
+
+// // Remove all unnecessary group elements
+// allGroups.exit().remove()
+
+    
+//  // shape helper to build arcs:
+// var arcGenerator = d3.arc()
+//   .innerRadius(0)
+//   .outerRadius(radius)
+
+// // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+// svg
+//   .selectAll('mySlices')
+//   .data(data_ready)
+//   .enter()
+//   .append('path')
+//     .attr('d', arcGenerator)
+//     .attr('fill', function(d){ return(color(d.data.key)) })
+//     .attr("stroke", "black")
+//     .style("stroke-width", "2px")
+//     .style("opacity", 0.7)
+
+// // Now add the annotation. Use the centroid method to get the best coordinates
+// svg
+//   .selectAll('mySlices')
+//   .data(data_ready)
+//   .enter()
+//   .append('text')
+//   .text(function(d){ return "grp " + d.data.key})
+//   .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
+//   .style("text-anchor", "middle")
+//   .style("font-size", 17) 
+
+// const arc = d3.arc() 
+//   .innerRadius(0) 
+//   .outerRadius(100); 
+  
+// // Grouping different arcs 
+// const arcs = pieChart.selectAll("arc") 
+//   .data(pie(data)) 
+//   .enter() 
+//   .append("g")
+  
+// // Appending path  
+// arcs.append("path") 
+//   .attr("fill", (data, i)=>{ 
+//           let value=data.data; 
+//           return d3.schemeTableau10[i]; 
+//         }) 
+//   .attr("d", arc)
+//   .merge(arcs)
+//   .exit()
+// 	.remove(); 
+
+// Adding data to each arc 
+// u.append("text") 
+// 	.attr("transform",(d)=>{  
+//         	return "translate("+  
+//           u.centroid(d) + ")";  
+//         }) 
+//   .text(function(d){ 
+//           return d.value;  
+//         }); 
+
 }
